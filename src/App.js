983a8e9b066ac48963116cdf6be47c4537ba6d4b
StavDorkam <stavdorkam@gmail.com>
2019-09-04 16:46:53 +0300

    pushing untracked changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import HomePage from './pages/HomePage.component';
+import HomePage from './pages/home/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import NotFoundPage from './pages/notfound/notfoundpage.component';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/shop" component={ShopPage}/>
+      <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
