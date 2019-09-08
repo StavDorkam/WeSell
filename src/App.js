72026fe82f707d90d7a73429242bbc5c007bd8a8
StavDorkam <stavdorkam@gmail.com>
2019-09-08 13:14:52 +0300

    added user reducer and redirect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+
 import HomePage from './pages/home/home-page.component';
 import ShopPage from './pages/shop/shop-page.component';
 import SignInAndUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component';
 import NotFoundPage from './pages/notfound/notfound-page.component';
 import Header from './components/header/header.component';
-import {Switch, Route} from 'react-router-dom';
+
 import {auth, createUserProfileDoc} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDoc(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           })
         })
       } else {
-        this.setState({currUser: userAuth})
+        setCurrentUser(userAuth)
       }
     })
-
   }
 
   componentWillUnmount() {
@@ -44,11 +40,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header currUser={this.state.currUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndUpPage} />
+          <Route exact path="/signin" render={() => this.props.currUser ? (<Redirect to='/' />) : (<SignInAndUpPage />)} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
@@ -56,4 +52,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({user}) => ({
+  currUser: user.currUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import {userActionTypes} from './user.types';
+
 const INITIAL_STATE = {
     currUser: null
 }
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case userActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currUser: action.payload
