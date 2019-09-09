063e3382d25e655b02cfcec4b9cc7f3961ddb23e
StavDorkam <stavdorkam@gmail.com>
2019-09-09 15:56:27 +0300

    converted shop data array to hashtable, added shop selectors, and updated collection page, item, and overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
-import {selectShopCollections} from '../../redux/shop/shop.selector';
+import {selectShopCollectionsForPreview} from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
@@ -19,7 +19,7 @@ const CollectionsOverview = ({collections}) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectShopCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
